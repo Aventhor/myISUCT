e701ff573ffff77962f2fc2976ef3df284c062ad
Aventhor <vaster399@gmail.com>
2019-01-11 20:27:34 +0300

    login form

---
+++ lib/app.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:myisuct/splashscreen/splashscreen.dart';
+import 'package:myisuct/login/login.dart';
+
+class MyISUCTApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Мой ИГХТУ',
+      home: LoginPage(),
+      initialRoute: '/login',
+      onGenerateRoute: _getRoute,
+      theme: _isuctTheme,
+    );
+  }
+
+  Route<dynamic> _getRoute(RouteSettings settings) {
+    if(settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => LoginPage(),
+      fullscreenDialog: true,
+    );
+  }
+}
+final ThemeData _isuctTheme = _buildisuctTheme();
+
+ThemeData _buildisuctTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    primaryColor: Colors.redAccent,
+    accentColor: Colors.redAccent,
+    buttonTheme: base.buttonTheme.copyWith(
+      buttonColor: Colors.redAccent,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    scaffoldBackgroundColor: Colors.white,
+    cardColor: Colors.white30,
+    textSelectionColor: Colors.yellow,
+    errorColor: Colors.red,
+    textTheme: _buildisuctTextTheme(base.textTheme),
+    primaryTextTheme: _buildisuctTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildisuctTextTheme(base.accentTextTheme),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+    )
+  );
+}
+TextTheme _buildisuctTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline: base.headline.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    title: base.title.copyWith(
+      fontSize: 18.0,
+      fontWeight: FontWeight.bold,
+    ),
+    caption: base.caption.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+  ).apply(
+    displayColor: Colors.black,
+    bodyColor: Colors.black,
+  );
+}
---
+++ lib/login/login.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('images/isuct_logo.png', width: 120.0, height: 120.0),
+                SizedBox(height: 20.0),
+                Text('Мой ИГХТУ')
+              ],
+            ),
+            SizedBox(height: 100.0),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Номер студенческого',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Номер паспорта',
+              ),
+              obscureText: true,
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Отмена'),
+                  onPressed: () {
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Войти'),
+                  elevation: 6.0,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
