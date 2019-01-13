6434b3025b5cc2d87d1628883b53e457b912ee98
Aventhor <vaster399@gmail.com>
2019-01-13 17:05:36 +0300

    drawer test

--- lib/home.dart
+++ lib/home.dart
@@ -4,45 +4,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-    appBar: new AppBar(
-      title: new Text("Мой ИГХТУ"),
-      actions: <Widget>[
-        new IconButton(
-          icon: new Icon(Icons.contacts),
-          onPressed: () {},
-        )
-      ],
-    ),
-    drawer: new Drawer(
-      child: new ListView(
-        children: <Widget>[
-          new DrawerHeader(
-            child: new Text("Drawer Header"),
-            decoration: new BoxDecoration(
-              color: Colors.blue,
-            ),
-          ),
+      appBar: new AppBar(
+        title: new Text("Мой ИГХТУ"),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.contacts),
+            onPressed: () {},
+          )
         ],
       ),
-    ),
-
-    bottomNavigationBar: new BottomNavigationBar(items: [
-      new BottomNavigationBarItem(
-        icon: new Icon(Icons.chat),
-        title: new Text("Новости"),
+      drawer: new Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: const Text('Trevor Widget'),
+              accountEmail: const Text('trevor.widget@example.com'),
+              currentAccountPicture: const CircleAvatar(),
+            ),
+            new ListTile(
+              title: Text('Выйти'),
+              leading: new IconButton(
+                icon: new Icon(Icons.exit_to_app),
+                onPressed: () {},
+                ),
+              onTap: () {
+              Navigator.of(context).pushNamed('/login');
+              }
+            ),
+         ],
+        ),
       ),
 
-      new BottomNavigationBarItem(
-        icon: new Icon(Icons.event),
-        title: new Text("Расписание"),
-      ),
+      bottomNavigationBar: new BottomNavigationBar(items: [
+        new BottomNavigationBarItem(
+          icon: new Icon(Icons.chat),
+          title: new Text("Новости"),
+          
+        ),
 
-      new BottomNavigationBarItem(
-        icon: new Icon(Icons.face),
-        title: new Text("Профиль"),
-      )
-    ]
-    ),
-  );
-}
+        new BottomNavigationBarItem(
+          icon: new Icon(Icons.event),
+          title: new Text("Расписание"),
+        ),
+
+        new BottomNavigationBarItem(
+          icon: new Icon(Icons.face),
+          title: new Text("Профиль"),
+        )
+      ]
+      ),
+    );
+  }
 }
